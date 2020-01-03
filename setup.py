ba87301bedfefaefaa29710dddc0648df238f31e
Alexander Korzun <korzun.sas@mail.ru>
2020-01-03 20:29:16 +0300

    Add py.typed file

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup
 
 setup(
     name = 'katescript',
-    version = '1.0.0',
+    version = '1.0.1',
     license = 'CC0',
     description = 'A simple scripting language',
+    zip_safe = False,
     packages = ['kates'],
     tests_require = ['pytest'],
     setup_requires = ['pytest-runner'],
+    package_data = {'kates': ['py.typed']},
 )
